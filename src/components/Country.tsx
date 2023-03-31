0163aab408a618d70a79af09b506664b0d7b08f9
kvitorr <kvitorrsantos@gmail.com>
2023-03-31 06:22:04 -0300

    Att Country: Add CountryDetails component

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import formatNumber from '../utils/formatString'
 import './Country.css'
+import ReactDOM from 'react-dom';
+import CountryDetails from './CountryDetails';
 
 interface ICountryProps {
     country: any
 }
 
 
-const Country: React.FC<ICountryProps> = ({country}) => {
-    console.log(country)
+const Country: React.FC<ICountryProps> = ({country}) =>   {  
+
+    const [showDetails, setShowDetails] = useState(false);
+
     return (
         <>
-            <div className='countryBox'>
+            <div className='countryBox' onClick={() => setShowDetails(!showDetails)}>
                 <div className='countryImage'>
                     <img src={country.flags.svg} alt={country.flags.alt} />
                 </div>
@@ -21,11 +26,18 @@ const Country: React.FC<ICountryProps> = ({country}) => {
                         <p className='countryPopulationName'>{country.name.common}</p>
                     </div>
 
-                    <button className='learnMoreBtn'>
+                    <button className='learnMoreBtn' onClick={() => setShowDetails(!showDetails)}>
                         Learn More
                     </button>
                 </div>
             </div>
+
+            {showDetails && (
+                ReactDOM.createPortal(
+                    <CountryDetails country={country} setShowDetails={setShowDetails}/>,
+                    document.body
+                )
+            )}
         </>
     )
 }
